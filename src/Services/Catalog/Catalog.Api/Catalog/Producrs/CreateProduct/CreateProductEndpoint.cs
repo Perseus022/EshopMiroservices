8d077b2c5edc6e071234d1555a9e88beea8b85c3
Perseus022 <sperror@gmail.com>
2026-03-23 15:35:53 +0200

    CQRS and Create product Endpoint and Handler

---
+++ src/Services/Catalog/Catalog.Api/Catalog/Producrs/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,31 @@
+﻿namespace Catalog.Api.Catalog.Producrs.CreateProduct
+{
+    public record CreateProductRequest(
+        string Name, List<string> Category, string Description, string ImageFile, decimal Price
+    );
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        void ICarterModule.AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender send) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await send.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .WithSummary("Create a new product")
+            .WithDescription("Creates a new product in the catalog.")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Catalog/Producrs/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.Api.Catalog.Producrs.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name, List<string> Category, string Description, string ImageFile, decimal Price
+        ) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            // Here you would typically save the product to a database
+            // For this example, we'll just return the created product's ID
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
